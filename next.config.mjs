c992c63d6ed0ba60e70b1ca9ecc57a1fc7e60146
calvinorr <calvin.orr@gmail.com>
2025-09-27 16:25:06 +0100

    Fix React version conflict and Next.js config

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+@@ .. @@
+-import type { NextConfig } from "next";
+-
+-const nextConfig: NextConfig = {
++/** @type {import('next').NextConfig} */
++const nextConfig = {
+   /* config options here */
+ };
+ 
+ export default nextConfig;
